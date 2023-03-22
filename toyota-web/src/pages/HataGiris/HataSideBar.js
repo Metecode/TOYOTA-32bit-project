23b9add123ff4eaabfc3547b129ee5c7893e20fe
Mete Uçar <ee09mete@gmail.com>
2023-03-22 13:56:35 +0300

    sidebar button, checkbox ve textfield eklendi.

--- toyota-web/src/pages/HataGiris/HataSideBar.js
+++ toyota-web/src/pages/HataGiris/HataSideBar.js
@@ -4,16 +4,12 @@ import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 import Checkbox from "@mui/material/Checkbox";
-import IconButton from "@mui/material/IconButton";
-import CommentIcon from "@mui/icons-material/Comment";
-import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Stack from "@mui/material/Stack";
+import "../../fonts/hataGiris.css"
+import TextField from '@mui/material/TextField';
 
 export default function HataSideBar() {
   const [checked, setChecked] = React.useState([0]);
@@ -34,37 +30,51 @@ export default function HataSideBar() {
     <Box>
       <List>
         <Stack disablePadding spacing={2} direction="row">
-            <FormControlLabel style={{marginLeft:2}} control={<Checkbox />} label="Harigami" />
-            <FormControlLabel control={<Checkbox />} label="RDD" />        
+            <FormControlLabel style={{marginLeft:2}} control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 28 } }} />} label="Harigami" />
+            <FormControlLabel control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: 28 } }} />} label="RDD" />        
         </Stack>
         <ListItem  >
-          <ListItemButton disabled  style={{border:"1px solid black", borderRadius:"10px"}}>
+          <ListItemButton disabled className="Button">
             <ListItemText primary={"HIZLI KAYDET"} />
           </ListItemButton>
         </ListItem>
-        <ListItem  disablePadding>
-          <ListItemButton disabled>
+        <ListItem  >
+          <ListItemButton disabled className="Button">
             <ListItemText primary={"KAYDET VE GEÇ"} />
           </ListItemButton>
         </ListItem>
-        <ListItem disablePadding>
-          <ListItemButton disabled>
+        <ListItem  >
+          <ListItemButton disabled className="Button">
             <ListItemText primary={"HATA KAYIT"} />
           </ListItemButton>
         </ListItem>
       </List>
       <Divider />
       <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
+      
+      <ListItem >
+      <TextField id="outlined-basic" label="MONTAJ NO" defaultValue={'222'} variant="outlined" />
+          </ListItem>
+          <ListItem >
+            <ListItemButton className="Button">
+              <ListItemText primary={"ARA"} />
             </ListItemButton>
           </ListItem>
-        ))}
+          <ListItem >
+            <ListItemButton className="Button">
+              <ListItemText primary={"TERMİNAL İLK RESMİ"} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem >
+            <ListItemButton className="Button">
+              <ListItemText primary={"SIK GELEN HATA"} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem >
+            <ListItemButton className="Button">
+              <ListItemText primary={"MANİFEST"} />
+            </ListItemButton>
+          </ListItem>
       </List>
     </Box>
   );
