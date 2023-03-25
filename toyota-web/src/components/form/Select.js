1f7ec08eb2477b24396c4acafab1a20982538dcf
Mete Uçar <ee09mete@gmail.com>
2023-03-25 14:08:13 +0300

    select componentinin up down butonunda stil degisikligi yapildi

--- toyota-web/src/components/form/Select.js
+++ toyota-web/src/components/form/Select.js
@@ -6,6 +6,9 @@ import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import { useField, ErrorMessage } from "formik";
 import { useEffect, useState, useRef } from "react";
+import Button from '@mui/material/Button';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 export default function Input({
   label,
   options,
@@ -60,20 +63,26 @@ export default function Input({
           id="demo-simple-select-error"
           size="small"
           label={label}
+          style={{display:"block", float: "left"}}
           // value={menuItems[0].value}
           // defaultValue={menuItems[0] ? menuItems[0].value : 'seciniz'}
           {...props}
           onChange={changeHandle}
         >
-          {dropDown && <button onClick={scrollToDown}>Scroll to down</button>}
-          {dropDown && <button onClick={scrollToUp}>Scroll to up</button>}
-
+          
+          <FormControl style={{float:"right"}}>
+          {dropDown && <Button size="large" onClick={scrollToUp}><ArrowDropUpIcon/></Button>}
+          {dropDown && <Button size="large" onClick={scrollToDown}> <ArrowDropDownIcon/> </Button>}
+      </FormControl>
+         
           {menuItems.map((option, key) => (
             <MenuItem value={option.key} key={key}>
               {option.value}
             </MenuItem>
           ))}
+          
         </Select>
+        
         <FormHelperText>Required</FormHelperText>
       </FormControl>
       {/* <select  className="w-full  border-b outline-none focus:border-black" onChange={changeHandle} defaultValue={field.value} {...props}>
