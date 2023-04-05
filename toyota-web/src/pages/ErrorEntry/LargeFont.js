fac176e75b6cfe5f799c53557271e8aa350a0474
Mete Uçar <ee09mete@gmail.com>
2023-04-06 02:53:32 +0300

    delete junk file

--- toyota-web/src/pages/ErrorEntry/LargeFont.js
+++ toyota-web/src/pages/ErrorEntry/LargeFont.js
@@ -1,22 +1,14 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
-import ListItemText from "@mui/material/ListItemText";
-import ListItem from "@mui/material/ListItem";
-import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import CloseIcon from "@mui/icons-material/Close";
 import Slide from "@mui/material/Slide";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import Grid from '@mui/material/Grid';
+import Grid from "@mui/material/Grid";
 import {
   forwardRef,
   useImperativeHandle,
@@ -33,19 +25,19 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 });
 
 const LargeFont = forwardRef(function LargeFont({}, ref) {
-  const [info, setInfo]= useState([]);
-  const [info2, setInfo2]= useState([]);
+  const [info, setInfo] = useState([]);
+  const [info2, setInfo2] = useState([]);
   const [open, setOpen] = React.useState(false);
-  useEffect(()=>{
+  useEffect(() => {
     axios
-    .get("../db/largeFont.json")
+      .get("../db/largeFont.json")
       .then((res) => setInfo(res.data.data))
       .catch((err) => console.log(err));
-      axios
-    .get("../db/largeFont2.json")
+    axios
+      .get("../db/largeFont2.json")
       .then((res) => setInfo2(res.data.data[0]))
       .catch((err) => console.log(err));
-    },[])
+  }, []);
   useImperativeHandle(ref, () => ({
     openDialog: openDialog,
   }));
@@ -56,7 +48,7 @@ const LargeFont = forwardRef(function LargeFont({}, ref) {
   const handleClose = () => {
     setOpen(false);
   };
-  console.log(info)
+  console.log(info);
   return (
     <div>
       <Dialog
@@ -75,58 +67,66 @@ const LargeFont = forwardRef(function LargeFont({}, ref) {
             margin: "20px",
             backgroundColor: "white",
             borderRadius: "10px",
-            flexDirection: 'row' 
+            flexDirection: "row",
           }}
         >
           <HataHeaderBar />
         </Box>
-        <Grid  container spacing={2}>
-        <Grid style={{textAlign:"center"}} item xs={8}>
-        <Typography  style={{ fontSize: 100,fontWeight:"bold"}} >
-          {info2.modelCode} - 222
-          <br />
-          25073
-        </Typography>
-        </Grid>
-        <Grid item xs={4} >
-          <Card
-            sx={{ width: 270, height: 200, float: "right", margin: "20px", marginTop:"3px" }}
-          >
-            
-            <CardActions>
-              <Button
-                onClick={handleClose}
-                size="large"
-                variant="contained"
-                color="error"
-                style={{ width: 250 }}
-              >
-                HATA GİRİŞİ
-              </Button>
-            </CardActions>
-            <CardContent>
-              <TextField
-                style={{ marginLeft: "7px" }}
-                id="outlined-basic"
-                label="MONTAJ NO"
-                value={222}
-                variant="outlined"
-              />
-            </CardContent>
-            <CardActions>
-              <Button size="large" variant="outlined" style={{ width: 250 }}>
-                ARA
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
+        <Grid container spacing={2}>
+          <Grid style={{ textAlign: "center" }} item xs={8}>
+            <Typography style={{ fontSize: 100, fontWeight: "bold" }}>
+              {info2.modelCode} - 222
+              <br />
+              25073
+            </Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <Card
+              sx={{
+                width: 270,
+                height: 200,
+                float: "right",
+                margin: "20px",
+                marginTop: "3px",
+              }}
+            >
+              <CardActions>
+                <Button
+                  onClick={handleClose}
+                  size="large"
+                  variant="contained"
+                  color="error"
+                  style={{ width: 250 }}
+                >
+                  HATA GİRİŞİ
+                </Button>
+              </CardActions>
+              <CardContent>
+                <TextField
+                  style={{ marginLeft: "7px" }}
+                  id="outlined-basic"
+                  label="MONTAJ NO"
+                  value={222}
+                  variant="outlined"
+                />
+              </CardContent>
+              <CardActions>
+                <Button size="large" variant="outlined" style={{ width: 250 }}>
+                  ARA
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
         </Grid>
         {info.map((item) => {
-              return (
-        <Typography style={{ fontSize: 30,marginLeft:"10px",fontWeight:"bold"}} >
-          {item.partName} - {item.defectName}
-        </Typography>
-              )})}
+          return (
+            <Typography
+              style={{ fontSize: 30, marginLeft: "10px", fontWeight: "bold" }}
+            >
+              {item.partName} - {item.defectName}
+            </Typography>
+          );
+        })}
       </Dialog>
     </div>
   );
